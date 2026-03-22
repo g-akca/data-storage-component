91fa5d858433c81bb3138de972d7518848961184
g-akca <gok.akca66@gmail.com>
2026-03-22 03:54:12 +0300

    Fix bg image url

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div 
       className="
         text-base leading-base text-blue-200 bg-blue-900 tablet:bg-blue-950 
-        bg-[url('images/bg-mobile.png')] tablet:bg-[url('images/bg-desktop.png')] 
+        bg-[url('/images/bg-mobile.png')] tablet:bg-[url('/images/bg-desktop.png')] 
         min-h-screen bg-cover tablet:bg-no-repeat tablet:bg-bottom-left  
         tablet:bg-size-[100%_auto] p-6 flex justify-center items-center
       "
